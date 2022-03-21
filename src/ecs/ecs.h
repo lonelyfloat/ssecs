f415696b48b5878377d35ac25870d46c1fae1c66
lonelyfloat <92132666+lonelyfloat@users.noreply.github.com>
2022-03-21 19:36:55 -0400

    removed unnecessary include, added comments

--- src/ecs/ecs.h
+++ src/ecs/ecs.h
@@ -3,7 +3,6 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-#include <raylib.h>
 
 typedef uint32_t EntityID;
 
@@ -16,10 +15,10 @@ typedef enum ComponentType
 
 typedef struct ComponentData
 {
-    void* data;
-    EntityID *dense; // Get IDs by using index here
+    void* data;       // Component data
+    EntityID *dense;  // Get IDs by using index here
     EntityID *sparse; // Get indices by using ID here
-    uint32_t count;
+    uint32_t count;   // how many elements there are in dense array
 } ComponentData;
 
 typedef struct EntityData
@@ -32,21 +31,13 @@ void InitEntityData(EntityData *data, int entityCount,int componentCount, size_t
 void FreeEntityData(EntityData *data);
 
 bool HasComponent(EntityData *data, ComponentType type, EntityID ID);
-//void AddComponent(EntityData *data, ComponentType type, EntityID ID, void* addedData);
-// (((char*)data->componentData[type].data) + (ID * sizes[type]))
+
 #define AddComponent(entData, ID, type, val, valType)                                                  \
     do {                                                                                               \
     (entData)->componentData[type].dense[(entData)->componentData[type].count] = ID;                   \
     (entData)->componentData[type].sparse[ID] = (entData)->componentData[type].count;                  \
     ((valType*)(entData)->componentData[type].data)[(entData)->componentData[type].count] = val;       \
     ++(entData)->componentData[type].count;}while(0) 
-    
-//((valType *)((entData)->componentData[type].data))[ID];
-
-/* fix these later!
-void SetECSData(EntityData *data, uint8_t ID, ComponentType type, void *in);
-void GetECSData(EntityData *data, int ID, ComponentType type, void *out);
-*/
 
 // systems
 

0c690eba10e4292f67b1fd4a0a8b6cdca59f8d9f
lonelyfloat <92132666+lonelyfloat@users.noreply.github.com>
2022-03-21 19:41:00 -0400

    removed unnecessary stuff

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <raylib.h>
-#include <stdio.h>
 #include "ecs/ecs.h"
 #include "ecs/components.h"
 
@@ -71,8 +70,7 @@ void UpdateDrawFrame(void)
             ClearBackground(RAYWHITE);
             DrawFPS(0,0);
             DrawEntities(&entityData);
-            //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+    
         EndDrawing();
         //----------------------------------------------------------------------------------
 
